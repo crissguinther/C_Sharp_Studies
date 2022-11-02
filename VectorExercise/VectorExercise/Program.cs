d4feebadd30bef66ca23c62b73228597ed552a4e
Criss Guinther <crissguinther@proton.me>
2022-11-02 16:07:20 -0300

    Vector Exercise

---
+++ VectorExercise/VectorExercise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room[] vect = new Room[10];
+            Console.Write("Enter the ammount of rooms to rent: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\n Rent #" + (i + 1));
+                Console.Write("Enter student name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter student email: ");
+                string email = Console.ReadLine();
+                Boolean isRent = false;
+
+                while (!isRent)
+                {
+                    Console.Write("Which room student chose: ");
+                    int r = int.Parse(Console.ReadLine());
+                    if (vect[r] == null)
+                    {
+                        vect[r] = new Room(name, email);
+                        isRent = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Room is already rented!");
+                    }
+                }
+            }
+
+            for (int i = 0; i < vect.Length; i++)
+            {
+                if (vect[i] != null) Console.WriteLine("\n Room #" + i + ": " + vect[i].ToString());
+
+            }
+        }
+    }
+}
---
+++ VectorExercise/VectorExercise/Room.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Program
+{
+    internal class Room
+    {
+        public string Name { get; set;}
+        public string Email { get; set; }
+
+        public Room() { }
+
+        public Room(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"Room rented by {Name}, using {Email} as email";
+        }
+    }
+}

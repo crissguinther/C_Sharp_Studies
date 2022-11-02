8a3e23b845bf8b5ffe52b7bfabfd92b2e88643f8
Criss Guinther <crissguinther@proton.me>
2022-11-02 14:59:57 -0300

    reference and value types

---
+++ Struct/Struct/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal struct Point
+    {
+        public double x;
+        public double y;
+
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+
+
+    }
+}
---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We don't need to start with new command, even though
+            // he accepts it.
+            Point p;
+
+            p.x = 10;
+            p.y = 20;
+
+            Console.WriteLine(p);
+
+            // This time, will be initialized with default values
+            p = new Point();
+
+            Console.WriteLine(p);
+        }
+    }
+}

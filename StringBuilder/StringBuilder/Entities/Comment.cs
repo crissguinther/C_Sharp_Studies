f305b5775097c1f4108ac838e2796ec9536ea55a
Criss Guinther <crissguinther@proton.me>
2022-11-14 09:32:34 -0300

    String Builder

---
+++ StringBuilder/StringBuilder/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace Program.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}

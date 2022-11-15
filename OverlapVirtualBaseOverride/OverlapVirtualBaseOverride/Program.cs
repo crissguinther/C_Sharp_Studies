233e18305397bc5a56bfe158ebf09f1668896be4
Criss Guinther <crissguinther@proton.me>
2022-11-15 17:19:18 -0300

    Overlap, virtual, base and override

---
+++ OverlapVirtualBaseOverride/OverlapVirtualBaseOverride/Program.cs
@@ -0,0 +1,19 @@
+﻿using Program.Entities;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 500.0);
+            SavingsAccount sacc = new SavingsAccount(1002, "Maria", 500.0, 0.01);
+
+            acc.Withdraw(10.0);
+            sacc.Withdraw(10.0);
+
+            Console.WriteLine(acc.Balance);
+            Console.WriteLine(sacc.Balance);
+        }
+    }
+}
